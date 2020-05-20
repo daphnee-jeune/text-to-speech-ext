d9be2ef15fbe4097ac3e73dc555911d1a97967c2
Daphnee <daphneejeune.dev@gmail.com>
2020-05-19 22:15:18 -0400

    file name change

---
+++ popup.js
@@ -0,0 +1,46 @@
+const playButton = document.querySelector('#playBtn')
+const pauseButton = document.querySelector('#pauseBtn')
+const stopButton = document.querySelector('#stopBtn')
+const textInput = document.querySelector('#text')
+const speedInput = document.querySelector('#speed')
+let currentCharacter
+
+playButton.addEventListener('click', () => {
+  playText(textInput.value)
+})
+pauseButton.addEventListener('click', pauseText)
+stopButton.addEventListener('click', stopText)
+
+speedInput.addEventListener('input', () => {
+  stopText()
+  playText(utterance.text.substring(currentCharacter))
+})
+
+const utterance = new SpeechSynthesisUtterance()
+utterance.addEventListener('end', () => {
+  textInput.disabled = false
+})
+utterance.addEventListener('boundary', e => {
+  currentCharacter = e.charIndex
+})
+
+function playText(text) {
+  if (speechSynthesis.paused && speechSynthesis.speaking) {
+    return speechSynthesis.resume()
+  }
+
+  if (speechSynthesis.speaking) return
+  utterance.text = text
+  utterance.rate = speedInput.value || 1
+  textInput.disabled = true
+  speechSynthesis.speak(utterance)
+}
+
+function pauseText() {
+  speechSynthesis.speaking && speechSynthesis.pause()
+}
+
+function stopText() {
+  speechSynthesis.resume()
+  speechSynthesis.cancel()
+}
